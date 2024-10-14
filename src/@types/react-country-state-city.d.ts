db4f3ba23d1181d34b67c848ee95d74c3634f149
codingking00 <danny.mengen.li@gmail.com>
2024-10-15 00:48:15 +1100

    my website

---
+++ src/@types/react-country-state-city.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-country-state-city' {
+    export const CountrySelect: React.ComponentType<any>;
+    export const StateSelect: React.ComponentType<any>;
+    export const CitySelect: React.ComponentType<any>;
+  }
+  
